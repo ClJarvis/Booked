7abbd939291675d5a44486d0612b2f65d7138525
Chris <clj@ollure.com>
2019-05-30 10:32:15 -0500

    updated

--- login.js
+++ login.js
@@ -2,8 +2,10 @@ function validate() {
  	var username = document.getElementById("username").value;
  	var password = document.getElementById("password").value;
     if (username === "BOB" && password === "2525"){
-		alert ("Login successfully");
-	window.location = "success.html"; // Redirecting to other page.
+		
+	window.location = "success.html"; 
 	return false;
+	} else {
+	alert("Try again!");
 	}
 }

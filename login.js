800ed9a52882cc8fb9d719599f5c7a434e0a97f2
Chris <clj@ollure.com>
2019-05-30 12:13:25 -0500

    added password reveal toggle

--- login.js
+++ login.js
@@ -1,11 +1,20 @@
 function validate() {
  	var username = document.getElementById("username").value;
- 	var password = document.getElementById("password").value;
+ 	var password = document.getElementById("Apassword").value;
     if (username === "BOB" && password === "2525"){
 		
 	window.location = "success.html"; 
 	return false;
 	} else {
 	alert("Try again!");
+	}
+}
+
+function showPassword() {
+	var x = document.getElementById("Apassword");
+	if (x.type === "password") {
+  	  x.type = "text";
+	} else {
+	  x.type = "password";
 	}
 }
